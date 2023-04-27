1f04cab0c30675f32ed124adf9eaac3f3e8927d2
leoniddavidchuk <49954415+LeonidDavidchuk@users.noreply.github.com>
2023-04-27 15:11:26 +0300

    add login page and adaptive for other devices

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Registration from "./pages/Registration/Registration.jsx";
+import Login from "./pages/Login/Login.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/registration" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import styles from "./Registration.module.css";
+import styles from "./Login.module.css";
 import ednipro_logo from "../Registration/images/ednipro_logo.svg";
 import child from "../Registration/images/child.svg";
 import child_mobile from "../Registration/images/child_mobile.svg";
 import child_tablet from "../Registration/images/child_tablet.svg";
 
-function Registration() {
+function Login() {
   return (
     <div className={styles.container}>
       <img src={child} className={styles.child_photo} alt="child" />
@@ -19,9 +19,8 @@ function Registration() {
         className={styles.child_tablet}
         alt="child tablet"
       />
-
       <div className={styles.container__right}>
-        <a href="/registration">
+        <a href="/login">
           <img
             className={styles["logo-top-right"]}
             src={ednipro_logo}
@@ -31,14 +30,14 @@ function Registration() {
 
         <div className={styles.content}>
           <div className={styles.registration_text}>
-            <p>Реєстрація</p>
+            <p>Авторизація</p>
           </div>
           <div className={styles.forms_all}>
             <div className={styles.forms}>
               <input
                 className={styles.form}
                 type="text"
-                placeholder="Прізвище та Ім'я "
+                placeholder="E-mail : "
               />
               <input
                 className={styles.form}
@@ -46,16 +45,12 @@ function Registration() {
                 placeholder="Пароль : "
               />
             </div>
+            <div className={styles.remember_password}>
+              <input type="checkbox" />
+              <label>Запам'ятати мене</label>
+            </div>
             <div className={styles.form2}>
-              <input
-                className={styles.form}
-                type="text"
-                placeholder="E-mail : "
-              />
-              <button className={styles.button_registration}>
-                {" "}
-                Зареєструватися{" "}
-              </button>
+              <button className={styles.button_registration}> Увійти </button>
             </div>
           </div>
         </div>
@@ -64,4 +59,4 @@ function Registration() {
   );
 }
 
-export default Registration;
+export default Login;
